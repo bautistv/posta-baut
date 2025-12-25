fe436cc26a79ebbdc3216ea6975e7d267ba35bbf
bautistv <vbf.bautista@gmail.com>
2025-12-26 01:47:46 +0800

    combine sends

--- pkg/messenger/send.go
+++ pkg/messenger/send.go
@@ -5,35 +5,57 @@ import (
 	"fmt"
 )
 
-// Send sends a message using the provided Messenger based on the MessageType.
-func Send(ctx context.Context, m Messenger, msg TeamsMessage) error {
-	if m == nil {
-		return fmt.Errorf("messenger cannot be nil")
+func sendToChannel(ctx context.Context, m Messenger, msg SendMessageRequest) error {
+	switch {
+		case m == nil:
+			return fmt.Errorf("messenger cannot be nil")
+		case msg.Content == "":
+			return fmt.Errorf("message content is empty")
+		case msg.Target.Channel == nil:
+			return fmt.Errorf("target channel is nil")
+		case msg.Target.Channel.ChannelID == "":
+			return fmt.Errorf("target channel ID missing")
+		case msg.Target.Channel.TeamID == "":
+			return fmt.Errorf("target team ID missing")
+
+		default:
+			err := m.SendChannelMessage(ctx, msg.Target.Channel.TeamID, msg.Target.Channel.ChannelID, msg.Content)
+			if err!=nil{
+				return fmt.Errorf("error sending channel message: %w", err)
+			}
+			return nil
 	}
+}
 
-	switch msg.Type {
-	case MessageTypeChat:
-		if msg.ChatID == "" {
-			return fmt.Errorf("missing chat ID from chat message")
-		}
-		if msg.Content == "" {
-			return fmt.Errorf("message content cannot be empty")
-		}
-		return m.SendChatMessage(ctx, msg.ChatID, msg)
+func sendToChat(ctx context.Context, m Messenger, msg SendMessageRequest) error {
+	switch {
+		case m == nil:
+			return fmt.Errorf("messenger cannot be nil")
+		case msg.Content == "":
+			return fmt.Errorf("message content is empty")
+		case msg.Target.Chat == nil:
+			return fmt.Errorf("target channel is nil")
+		case msg.Target.Chat.ChatID == "":
+			return fmt.Errorf("target channel ID missing")
 
-	case MessageTypeChannel:
-		if msg.TeamID == "" {
-			return fmt.Errorf("missing team ID from channel message")
-		}
-		if msg.ChannelID == "" {
-			return fmt.Errorf("missing channel ID from channel message")
-		}
-		if msg.Content == "" {
-			return fmt.Errorf("message content cannot be empty")
-		}
-		return m.SendChannelMessage(ctx, msg.TeamID, msg.ChannelID, msg)
+		default:
+			err := m.SendChatMessage(ctx, msg.Target.Chat.ChatID, msg.Content)
+			if err!=nil{
+				return fmt.Errorf("error sending chat message: %w", err)
+			}
+			return nil
+	}
+}
+
+func Send(ctx context.Context, m Messenger, msg SendMessageRequest) error {
+	switch {
+	case msg.Target.Channel != nil:
+		return sendToChannel(ctx, m, msg)
+
+	case msg.Target.Chat != nil:
+		return sendToChat(ctx, m, msg)
 
 	default:
-		return fmt.Errorf("unsupported message type: %s", msg.Type)
+		return fmt.Errorf("message target must be specified")
 	}
 }
