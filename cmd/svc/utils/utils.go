151abffacecc474e73c440daa5c6e8c3f143feda
bautistv <vbf.bautista@gmail.com>
2025-12-26 01:42:05 +0800

    Update utils tests:

--- cmd/svc/utils/utils.go
+++ cmd/svc/utils/utils.go
@@ -1,36 +1,54 @@
-// Package utils provides service-level utilities for the Posta Baut application.
 package utils
 
 import (
 	"fmt"
 
-	pb "github.com/bautistv/posta-baut/internal/pb/v1"
+	pbv1 "github.com/bautistv/posta-baut/internal/pb/v1"
 	"github.com/bautistv/posta-baut/pkg/messenger"
 )
 
-// ReqToMsg converts a SendMessageRequest to a domain-level Message.
-func ReqToMsg(req *pb.SendMessageRequest) (messenger.TeamsMessage, error) {
-	var msg messenger.TeamsMessage
+// ReqToMsg converts the protobuf SendMessageRequest into the domain SendMessageRequest.
+func ReqToMsg(pbReq *pbv1.SendMessageRequest) (*messenger.SendMessageRequest, error) {
+    if pbReq == nil {
+        return nil, fmt.Errorf("request cannot be nil")
+    }
 
-	switch req.MessageType.(type) {
-	case *pb.SendMessageRequest_ChannelMessage:
-		channelMsg := req.GetChannelMessage()
-		msg = messenger.TeamsMessage{
-			Type:      messenger.MessageTypeChannel,
-			Content:   channelMsg.GetContent(),
-			TeamID:    channelMsg.GetTeamId(),
-			ChannelID: channelMsg.GetChannelId(),
-		}
-	case *pb.SendMessageRequest_ChatMessage:
-		chatMsg := req.GetChatMessage()
-		msg = messenger.TeamsMessage{
-			Type:    messenger.MessageTypeChat,
-			Content: chatMsg.GetContent(),
-			ChatID:  chatMsg.GetChatId(),
-		}
-	default:
-		return msg, fmt.Errorf("unsupported message type: %v", req.GetMessageType())
-	}
+    if pbReq.Target == nil {
+        return nil, fmt.Errorf("target is required")
+    }
 
-	return msg, nil
+    var target messenger.MessageTarget
+
+    switch t := pbReq.Target.Target.(type) {
+    case *pbv1.MessageTarget_Channel:
+        if t.Channel == nil {
+            return nil, fmt.Errorf("channel target is nil")
+        }
+        target.Channel = &messenger.TeamsChannelTarget{
+            TeamID:    t.Channel.GetTeamId(),
+            ChannelID: t.Channel.GetChannelId(),
+            ThreadID:  t.Channel.GetThreadId(),
+        }
+    case *pbv1.MessageTarget_Chat:
+        if t.Chat == nil {
+            return nil, fmt.Errorf("chat target is nil")
+        }
+        target.Chat = &messenger.ChatTarget{
+            ChatID:          t.Chat.GetChatId(),
+            ReplyToMessageID: t.Chat.GetReplyToMessageId(),
+        }
+    default:
+        return nil, fmt.Errorf("unknown message target type: %T", t)
+    }
+
+    if pbReq.Content == "" {
+        return nil, fmt.Errorf("content cannot be empty")
+    }
+
+    domainReq := &messenger.SendMessageRequest{
+        Target:  target,
+        Content: pbReq.GetContent(),
+    }
+
+    return domainReq, nil
 }
